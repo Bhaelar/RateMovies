7c7269a700139c0f89ae2a6d6ea6098f2a31698d
Bhaelar <okenlabasit@gmail.com>
2020-12-17 08:24:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import InputForm from './components/InputForm';
+import DisplayMovies from './components/DisplayMovies';
 import './App.css';
+import {GlobalProvider} from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+    <div className="container">
+      <h1 className="display-4">Rate your favorite movies</h1>
+        <InputForm />
+        <DisplayMovies />
+      </div>
+      </GlobalProvider>
   );
 }
 
